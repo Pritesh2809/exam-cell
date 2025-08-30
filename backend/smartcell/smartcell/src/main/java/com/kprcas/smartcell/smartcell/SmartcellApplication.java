4be0abcbf675869ff4b3b3f5792894815487d647
Pritesh <rspritesh009@gmail.com>
2025-08-30 11:16:37 +0530

    First commit

---
+++ backend/smartcell/smartcell/src/main/java/com/kprcas/smartcell/smartcell/SmartcellApplication.java
@@ -0,0 +1,13 @@
+package com.kprcas.smartcell.smartcell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcellApplication.class, args);
+	}
+
+}
---
+++ backend/smartcell/smartcell/src/test/java/com/kprcas/smartcell/smartcell/SmartcellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kprcas.smartcell.smartcell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
